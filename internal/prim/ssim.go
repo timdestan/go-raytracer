1ec1e29082eb689c9a783e75a03ef793664e7d34
Tim Destan <tim.destan@gmail.com>
2025-09-27 20:44:40 -0400

    minor: fix variable names

--- internal/prim/ssim.go
+++ internal/prim/ssim.go
@@ -75,7 +75,7 @@ func SSIM(img1, img2 image.Image) (float64, error) {
 }
 
 func computeSSIMOnWindow(img1, img2 [][]rgb, xstart, ystart int, kernel []float64) float64 {
-	var r1_sum, r2_sum, g1_sum, g2_sum, b1_sum, b2_sum float64
+	var r1Sum, r2Sum, g1Sum, g2Sum, b1Sum, b2Sum float64
 	n := float64(kernelSize * kernelSize)
 
 	// TODO: I think we're supposed to add padding, so that we can apply the kernel on the edges of the image.
@@ -88,25 +88,25 @@ func computeSSIMOnWindow(img1, img2 [][]rgb, xstart, ystart int, kernel []float6
 			i1 := img1[x][y]
 			i2 := img2[x][y]
 
-			r1_sum += float64(i1.r) * w
-			g1_sum += float64(i1.g) * w
-			b1_sum += float64(i1.b) * w
+			r1Sum += float64(i1.r) * w
+			g1Sum += float64(i1.g) * w
+			b1Sum += float64(i1.b) * w
 
-			r2_sum += float64(i2.r) * w
-			g2_sum += float64(i2.g) * w
-			b2_sum += float64(i2.b) * w
+			r2Sum += float64(i2.r) * w
+			g2Sum += float64(i2.g) * w
+			b2Sum += float64(i2.b) * w
 		}
 	}
 
-	r1_avg := r1_sum / n
-	g1_avg := g1_sum / n
-	b1_avg := b1_sum / n
+	r1Avg := r1Sum / n
+	g1Avg := g1Sum / n
+	b1Avg := b1Sum / n
 
-	r2_avg := r2_sum / n
-	g2_avg := g2_sum / n
-	b2_avg := b2_sum / n
+	r2Avg := r2Sum / n
+	g2Avg := g2Sum / n
+	b2Avg := b2Sum / n
 
-	var r1_var, g1_var, b1_var, r2_var, g2_var, b2_var, r12_var, g12_var, b12_var float64
+	var r1Var, g1Var, b1Var, r2Var, g2Var, b2Var, r12Var, g12Var, b12Var float64
 
 	for k1 := range kernelSize {
 		for k2 := range kernelSize {
@@ -117,44 +117,44 @@ func computeSSIMOnWindow(img1, img2 [][]rgb, xstart, ystart int, kernel []float6
 			i1 := img1[x][y]
 			i2 := img2[x][y]
 
-			r1_var += w * square(float64(i1.r)-r1_avg)
-			g1_var += w * square(float64(i1.g)-g1_avg)
-			b1_var += w * square(float64(i1.b)-b1_avg)
+			r1Var += w * square(float64(i1.r)-r1Avg)
+			g1Var += w * square(float64(i1.g)-g1Avg)
+			b1Var += w * square(float64(i1.b)-b1Avg)
 
-			r2_var += w * square(float64(i2.r)-r2_avg)
-			g2_var += w * square(float64(i2.g)-g2_avg)
-			b2_var += w * square(float64(i2.b)-b2_avg)
+			r2Var += w * square(float64(i2.r)-r2Avg)
+			g2Var += w * square(float64(i2.g)-g2Avg)
+			b2Var += w * square(float64(i2.b)-b2Avg)
 
-			r12_var += w * (float64(i1.r) - r1_avg) * (float64(i2.r) - r2_avg)
-			g12_var += w * (float64(i1.g) - g1_avg) * (float64(i2.g) - g2_avg)
-			b12_var += w * (float64(i1.b) - b1_avg) * (float64(i2.b) - b2_avg)
+			r12Var += w * (float64(i1.r) - r1Avg) * (float64(i2.r) - r2Avg)
+			g12Var += w * (float64(i1.g) - g1Avg) * (float64(i2.g) - g2Avg)
+			b12Var += w * (float64(i1.b) - b1Avg) * (float64(i2.b) - b2Avg)
 		}
 	}
 
-	r1_var /= (n - 1)
-	g1_var /= (n - 1)
-	b1_var /= (n - 1)
+	r1Var /= (n - 1)
+	g1Var /= (n - 1)
+	b1Var /= (n - 1)
 
-	r2_var /= (n - 1)
-	g2_var /= (n - 1)
-	b2_var /= (n - 1)
+	r2Var /= (n - 1)
+	g2Var /= (n - 1)
+	b2Var /= (n - 1)
 
-	r12_var /= (n - 1)
-	g12_var /= (n - 1)
-	b12_var /= (n - 1)
+	r12Var /= (n - 1)
+	g12Var /= (n - 1)
+	b12Var /= (n - 1)
 
-	compute_ssim := func(avg1, avg2, var1, var2, covar float64) float64 {
+	computeSSIM := func(avg1, avg2, var1, var2, covar float64) float64 {
 		numerator := (2*avg1*avg2 + c1) * (2*covar + c2)
 		denominator := (avg1*avg1 + avg2*avg2 + c1) * (var1 + var2 + c2)
 		return numerator / denominator
 	}
 
-	red_ssim := compute_ssim(r1_avg, r2_avg, r1_var, r2_var, r12_var)
-	green_ssim := compute_ssim(g1_avg, g2_avg, g1_var, g2_var, g12_var)
-	blue_ssim := compute_ssim(b1_avg, b2_avg, b1_var, b2_var, b12_var)
+	redSSIM := computeSSIM(r1Avg, r2Avg, r1Var, r2Var, r12Var)
+	greenSSIM := computeSSIM(g1Avg, g2Avg, g1Var, g2Var, g12Var)
+	blueSSIM := computeSSIM(b1Avg, b2Avg, b1Var, b2Var, b12Var)
 
 	// Average over RGB
-	return (red_ssim + green_ssim + blue_ssim) / 3.0
+	return (redSSIM + greenSSIM + blueSSIM) / 3.0
 }
 
 func makeGaussianKernel() []float64 {
